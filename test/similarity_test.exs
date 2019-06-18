727358e7ce383338918ba0cd42c5aaf3dded490a
preciz <barna.kovacs.codes@gmail.com>
2019-06-18 12:29:47 +0200

    Test Similarity module

--- test/similarity_test.exs
+++ test/similarity_test.exs
@@ -2,7 +2,25 @@ defmodule SimilarityTest do
   use ExUnit.Case
   doctest Similarity
 
-  test "greets the world" do
-    assert Similarity.hello() == :world
+  test "Cosine similarity" do
+    assert Similarity.cosine([1,2], [1,2]) |> Float.round == 1
+
+    assert Similarity.cosine([5,0], [0,5]) |> Float.round == 0
+
+    assert Similarity.cosine([1,2], [-1,-2]) |> Float.round == -1
+  end
+
+  test "Euclidean dot product" do
+    assert Similarity.dot_product([1], [1]) == 1
+
+    assert Similarity.dot_product([11, 23, 41], [7,9,13]) == 817
+  end
+
+  test "Euclidean magnitude" do
+    assert Similarity.magnitude([2]) == 2
+
+    assert Similarity.magnitude([1,2]) == Similarity.magnitude([2,1])
+
+    assert Similarity.magnitude([1,2]) |> Float.round(3) == 2.236
   end
 end
