70efb104026e11d270e9d1a97953c8dad0274508
preciz <barna.kovacs.codes@gmail.com>
2019-06-20 19:51:26 +0200

    Remove duplicated line (should be in `package()` function)

--- mix.exs
+++ mix.exs
@@ -10,7 +10,6 @@ defmodule Similarity.MixProject do
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      licenses: "MIT",
       source_url: "https://github.com/preciz/similarity",
 
       name: "Similarity",
